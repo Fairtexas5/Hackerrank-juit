1344c5c5b388ebe9c05519f7767abb394148cbd4
Aditya Singh <asfairtexas@gmail.com>
2022-12-05 09:37:13 +0530

    Update P49.c

--- P49.c
+++ P49.c
@@ -2,7 +2,7 @@
 
 void div_rem(int n1, int n2, int *div, int *rem)
 {
-	*div = n1 / n2;
+    *div = n1 / n2;
     *rem = n1 % n2;
 } 
 
@@ -17,9 +17,9 @@ int main(void)
         div_rem(a, b, &div, &rem);
         printf("%i divided by %i = %i and remainder is %i\n", a, b, div, rem);
     }
+    else
     printf("Ouside the range");
   }
   else printf("Oustside the range");
   return 0;
 }
-
