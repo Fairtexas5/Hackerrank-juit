8eb3d519f6f5dda691fcf625cfeb7cf9e61d3d3b
Aditya Singh <asfairtexas@gmail.com>
2023-01-04 00:48:28 +0530

    STRING

---
+++ P57.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() {
+    char s[100], ch;
+    int count = 0;
+
+    scanf("%[^\n]%*c", s);
+    scanf("%c", &ch);
+
+    for (int i = 0; s[i] != '\0'; ++i) {
+        if (ch == s[i])
+            ++count;
+    }
+
+    printf("Frequency of %c = %d", ch, count);
+    return 0;
+}
