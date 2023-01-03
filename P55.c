bc388232c576665c59532c64459e5dbaa0315bc7
Aditya Singh <asfairtexas@gmail.com>
2023-01-04 01:34:00 +0530

    STRING

---
+++ P55.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() {
+    char s[100];
+    int length = 0;
+    scanf("%999[^\n]", s);
+    for (int i = 0; s[i] != '\0'; i++){
+        length++;
+    }
+    printf ("Length of the string: %d", length);
+    return 0;
+}
